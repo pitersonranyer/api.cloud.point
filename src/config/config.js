b17f098fc7ac270b0988198dd5f24cef3e2e8f9b
Piterson Ranyer <pitersonranyer@gmal.comr>
2020-09-14 23:02:37 -0300

    save 14/09 umbler

--- src/config/config.js
+++ src/config/config.js
@@ -1,25 +1,10 @@
 module.exports = {
-  development: {
-    database: {
-      host: "localhost",
-      port: 3306,
-      name: "pointdojogador",
-      dialect: "mysql",
-      user: "root",
-      password: "admin",
-      timezone: "+08:00"
-    },
-    secret: '1C3C7E1694F1E9DAD939399E87E5FFB5DF06B2327CA31B409CB3'
-  },
-  production: {
     database: {
       host: "mysql669.umbler.com",
-      port: 3306,
       name: "pointdojogador",
       dialect: "mysql",
       user: "pointdojogador",
       password: "xedsi5-soWpet-cextek",
       secret: process.env.JWT_SECRET
     }
-  }
 };
