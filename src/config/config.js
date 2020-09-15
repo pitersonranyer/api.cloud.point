e4e393e08c22e07cb69a1d508facb536f725b4dd
Piterson Ranyer <pitersonranyer@gmal.comr>
2020-09-14 22:27:23 -0300

    save 14/09 umbler

--- src/config/config.js
+++ src/config/config.js
@@ -1,11 +1,8 @@
 module.exports = {
     database: {
       host: "mysql669.umbler.com",
-      name: "pointdojogador",
-      dialect: "mysql",
       user: "pointdojogador",
       password: "xedsi5-soWpet-cextek",
-      secret: process.env.JWT_SECRET
+      database: "pointdojogador"
     }
- 
 };
