3de6f89f9ae2968e3fa3ac7b0c8a1d34cbcc682d
Piterson Ranyer <pitersonranyer@gmal.comr>
2020-09-15 14:46:10 -0300

    save 15/09

--- src/model/timeRodadaCartola.js
+++ src/model/timeRodadaCartola.js
@@ -3,40 +3,28 @@ const sequelize = require("../database/database");
 
 const TimeRodadaCartola = sequelize.define("timeRodadaCartola", {
   anoTemporada: {  // 2020, 2021, 2022
-    type: Sequelize.INTEGER,
-    allowNull: false,
     primaryKey: true,
-    references: {         // Fk RodadaCartola 1:n
-      model: 'RodadaCartola',
-      key: 'anoTemporada'
-    }
+    allowNull: false,
+    type: Sequelize.INTEGER
   },
 
   //Sequelize não deixou criar FK da tabela  RodadaCartola
   idRodada: {
-    type: Sequelize.INTEGER,
+    primaryKey: true,
     allowNull: false,
-    primaryKey: true
+    type: Sequelize.INTEGER
   },
 
   idUsuario: {
-    type: Sequelize.INTEGER,
-    allowNull: false,
     primaryKey: true,
-    references: {         // Fk usuario 1:n
-      model: 'Usuario',
-      key: 'id'
-    }
+    allowNull: false,
+    type: Sequelize.INTEGER
   },
 
   time_id: {
-    type: Sequelize.INTEGER,
-    allowNull: false,
     primaryKey: true,
-    references: {         // Fk TimeUsuarioCartola 1:n
-      model: 'TimeUsuarioCartola',
-      key: 'time_id'
-    }
+    allowNull: false,
+    type: Sequelize.INTEGER
   },
 
   statusPgto: {
