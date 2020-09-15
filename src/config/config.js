ed5eb988dff654d285ce7b8d67368f683bd25e5f
Piterson Ranyer <pitersonranyer@gmal.comr>
2020-09-14 22:49:21 -0300

    save 14/09 umbler

--- src/config/config.js
+++ src/config/config.js
@@ -3,7 +3,7 @@ const Sequelize = require("sequelize");
 const connection = new Sequelize('pointdojogador', 'pointdojogador', 'xedsi5-soWpet-cextek',
   {
     host: 'mysql669.umbler.com',
-    dialet: 'mysql'
+    dialect: 'mysql'
   }
 );
 
