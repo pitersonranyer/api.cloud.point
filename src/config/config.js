5e9db457da1685178bf7689b6e21217bc5892819
Piterson Ranyer <pitersonranyer@gmal.comr>
2020-09-14 22:47:52 -0300

    save 14/09 umbler

--- src/config/config.js
+++ src/config/config.js
@@ -1,11 +1,10 @@
-module.exports = {
-    database: {
-      host: "mysql669.umbler.com",
-      name: "pointdojogador",
-      dialect: "mysql",
-      user: "pointdojogador",
-      password: "xedsi5-soWpet-cextek",
-      secret: process.env.JWT_SECRET
-    },
-    secret: '1C3C7E1694F1E9DAD939399E87E5FFB5DF06B2327CA31B409CB3'
-};
+const Sequelize = require("sequelize");
+
+const connection = new Sequelize('pointdojogador', 'pointdojogador', 'xedsi5-soWpet-cextek',
+  {
+    host: 'mysql669.umbler.com',
+    dialet: 'mysql'
+  }
+);
+
+module.exports = connection;
