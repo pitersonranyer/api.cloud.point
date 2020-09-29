9489bab9d231cfde7feafb0040de84c818682145
Piterson Ranyer <pitersonranyer@gmal.comr>
2020-09-29 18:59:09 -0300

    save github 29/09

--- src/config/config.js
+++ src/config/config.js
@@ -2,26 +2,27 @@ module.exports = {
   development: {
     database: {
 
-      host: "mysql669.umbler.com",
-      name: "pointdojogador-u",
-      dialect: "mysql",
-      user: "piterson",
-      password: "sWXFuvbtpFA3V6i",
+// --> banco atual. 
+ //     host: "mysql669.umbler.com",
+ //     name: "pointdojogador-u",
+ //     dialect: "mysql",
+ //     user: "piterson",
+ //     password: "sWXFuvbtpFA3V6i",
 
 
-
+// --> banco antigo
 //      host: "mysql669.umbler.com",
 //      name: "pointdojogador",
 //      dialect: "mysql",
 //      user: "pointdojogador",
 //      password: "xedsi5-soWpet-cextek",
 
-//      host: "localhost",
-//      port: 3306,
-//      name: "pointdojogador",
-//      dialect: "mysql",
-//      user: "root",
-//      password: "admin"
+      host: "localhost",
+      port: 3306,
+      name: "pointdojogador",
+      dialect: "mysql",
+      user: "root",
+      password: "admin"
       
     },
     secret: '1C3C7E1694F1E9DAD939399E87E5FFB5DF06B2327CA31B409CB3'
