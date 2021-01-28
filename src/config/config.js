3903d210aafa9a6d5ce3ad4548a58fab0cd13efc
Piterson Ranyer <pitersonranyer@gmal.comr>
2021-01-28 13:51:20 -0300

    cancelar inscricao de times

--- src/config/config.js
+++ src/config/config.js
@@ -3,11 +3,11 @@ module.exports = {
     database: {
 
 // --> banco atual. 
-//      host: "mysql669.umbler.com",
-//      name: "pointdojogador-u",
-//      dialect: "mysql",
-//      user: "piterson",
-//      password: "sWXFuvbtpFA3V6i",
+      host: "mysql669.umbler.com",
+      name: "pointdojogador-u",
+      dialect: "mysql",
+      user: "piterson",
+      password: "sWXFuvbtpFA3V6i",
 
 
 // --> banco antigo
@@ -17,12 +17,12 @@ module.exports = {
 //      user: "pointdojogador",
 //      password: "xedsi5-soWpet-cextek",
 
-      host: "localhost",
-      port: 3306,
-      name: "pointdojogador",
-      dialect: "mysql",
-      user: "root",
-      password: "admin"
+//      host: "localhost",
+//      port: 3306,
+//      name: "pointdojogador",
+//      dialect: "mysql",
+//      user: "root",
+//      password: "admin"
       
     },
     secret: '1C3C7E1694F1E9DAD939399E87E5FFB5DF06B2327CA31B409CB3'
