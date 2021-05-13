cd46b3576e5bc402c98b18e8c78294effc7b7132
Piterson Ranyer <pitersonranyer@gmal.comr>
2021-05-13 16:06:41 -0300

    backup

--- src/config/config.js
+++ src/config/config.js
@@ -3,11 +3,11 @@ module.exports = {
     database: {
 
 // --> banco atual. UMBLER
-      host: "mysql669.umbler.com",
-      name: "pointdojogador-u",
-      dialect: "mysql",
-      user: "piterson",
-      password: "sWXFuvbtpFA3V6i",
+//      host: "mysql669.umbler.com",
+//      name: "pointdojogador-u",
+//      dialect: "mysql",
+//      user: "piterson",
+//      password: "sWXFuvbtpFA3V6i",
 
 
 // --> banco antigo
@@ -26,12 +26,12 @@ module.exports = {
 //      password: "afe01cee",
 //      secret: process.env.JWT_SECRET
 
-//      host: "localhost",
-//      port: 3306,
-//     name: "pointdojogador",
-//      dialect: "mysql",
-//      user: "root",
-//      password: "admin"
+      host: "localhost",
+      port: 3306,
+     name: "pointdojogador",
+      dialect: "mysql",
+      user: "root",
+      password: "admin"
       
     },
     secret: '1C3C7E1694F1E9DAD939399E87E5FFB5DF06B2327CA31B409CB3'
