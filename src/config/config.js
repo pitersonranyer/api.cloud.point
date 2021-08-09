c08bf09651acde86b462d2753d6518e2a8dcc2fd
Piterson Ranyer <pitersonranyer@gmal.comr>
2021-08-09 16:28:13 -0300

    ajustes pontuacao anuallll

--- src/config/config.js
+++ src/config/config.js
@@ -3,11 +3,11 @@ module.exports = {
     database: {
 
 // --> banco atual. UMBLER
-      host: "mysql669.umbler.com",
-      name: "pointdojogador-u",
-      dialect: "mysql",
-      user: "piterson",
-     password: "sWXFuvbtpFA3V6i",
+    host: "mysql669.umbler.com",
+    name: "pointdojogador-u",
+    dialect: "mysql",
+    user: "piterson",
+    password: "sWXFuvbtpFA3V6i",
 
 // --> UOL HOST
 // host: "pointdojogador.mysql.uhserver.com",
